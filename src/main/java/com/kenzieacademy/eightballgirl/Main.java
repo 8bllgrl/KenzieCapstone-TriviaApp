0e4db3bbb31cb337af5ad953ea9717872fab6891
Sage Belknap <sage.belknap@mykenzie.snhu.edu>
2022-09-21 19:14:09 -0600

    First public commit.

---
+++ src/main/java/com/kenzieacademy/eightballgirl/Main.java
@@ -0,0 +1,28 @@
+package com.kenzieacademy.eightballgirl;
+
+public class Main {
+
+    /*~ Java Capstone Project. ~*/
+    /*
+        This quiz game will test your knowledge!
+        See if you have what it takes to answer as many questions as you can. For each question you get correct,
+        your score will go up. If you don't answer correctly, your score will not rise.
+
+        Now the quiz question amount can be changed more easily!
+        Instead of going into the quiz class to change the number of questions, simply input the amount of
+        questions you desire.
+
+        NOTE:
+        Took some advice from my teacher and tried to shorten my constructor code by turning some actions into methods.
+        I hope this looks better as an object-oriented program now :)
+        The reason I made this quiz in a separate method like this is i was preparing myself to add visuals.
+        That didn't seem to happen. I'll add visuals if necessary.
+
+     */
+
+    public static void main(String[] args) {
+
+        new Quiz(10);
+
+    }
+}
---
+++ src/main/java/com/kenzieacademy/eightballgirl/clueapi/ClueDTO.java
@@ -0,0 +1,25 @@
+package com.kenzieacademy.eightballgirl.clueapi;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.List;
+
+/*~ ClueDTO is a Data Transfer Object.
+            This will become an object that Quiz uses to find question/answer pairs. ~*/
+public class ClueDTO {
+
+
+    @JsonProperty("clues")
+    private List<Clues> clues;
+
+    public List<Clues> getClues() {
+        return clues;
+    }
+
+    public void setClues(List<Clues> clues) {
+        this.clues = clues;
+    }
+}
+
+
+
